ee898cda69e27cf8d395fa41f7b2d703a9e32ae9
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-07 16:52:00 -0400

    feat: add a base template, about page and make some enchancements for the landing page

--- LangDiary/settings.py
+++ LangDiary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'LangDiary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "home/templates"],
+        'DIRS': [BASE_DIR / "home/templates", BASE_DIR / "LangDiary/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
