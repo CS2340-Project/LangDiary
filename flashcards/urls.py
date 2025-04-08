c189377269b365e021549970e8c89c71044fc9c5
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-07 23:31:55 -0400

    fix: fix the flashcards scrolling, mark reviewd, edit, delete + sort by num revision properly

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.index, name='flashcards.index'),
- path('create_flashcard/', views.create_flashcard, name='flashcards.create_flashcard'),
- path('update_flashcard/', views.update_flashcard, name='flashcards.update_flashcard'),
- path('delete_flashcard/', views.delete_flashcard, name='flashcards.delete_flashcard'),
- path('mark_reviewed/', views.mark_reviewed, name='flashcards.mark_reviewed'),
+    path('', views.index, name='flashcards.index'),
+    path('create/', views.create_flashcard, name='flashcards.create_flashcard'),
+    path('update/', views.update_flashcard, name='flashcards.update_flashcard'),
+    path('delete/', views.delete_flashcard, name='flashcards.delete_flashcard'),
+    path('mark-reviewed/', views.mark_reviewed, name='flashcards.mark_reviewed'),
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,50 +1,81 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse
 from .models import Flashcard
 
 def index(request):
     template_data = {'title': 'LangDiary - Flashcards'}
     flashcards = Flashcard.objects.all().order_by('num_revisions')
+
+    # Handle card navigation
+    card_id = request.GET.get('card_id')
+    current_card_index = 0
+
+    # Fix for empty queryset
+    if not flashcards.exists():
+        return render(request, 'flashcards/index.html', {
+            'flashcards': [],
+            'template_data': template_data,
+        })
+
+    # Select first card by default
+    current_card = flashcards.first()
+
+    if card_id:
+        try:
+            # Find the current card
+            current_card = flashcards.get(id=card_id)
+            # Find position in sequence
+            flashcard_list = list(flashcards)
+            current_card_index = flashcard_list.index(current_card)
+        except (Flashcard.DoesNotExist, ValueError, IndexError):
+            # Fall back to first card if error
+            current_card = flashcards.first()
+            current_card_index = 0
+
+    # Get previous and next card IDs
+    prev_card_id = flashcards[current_card_index - 1].id if current_card_index > 0 else None
+    next_card_id = flashcards[current_card_index + 1].id if current_card_index < len(flashcards) - 1 else None
+
+    # Handle edit mode
+    edit_card = None
+    if request.GET.get('action') == 'edit' and request.GET.get('id'):
+        edit_card = get_object_or_404(Flashcard, id=request.GET.get('id'))
+
     return render(request, 'flashcards/index.html', {
         'flashcards': flashcards,
+        'current_card': current_card,
         'template_data': template_data,
+        'prev_card_id': prev_card_id,
+        'next_card_id': next_card_id,
+        'edit_card': edit_card,
     })
 
-
-# TODO: make a proper business logic for creating/deleting/editing flashcards
-# TODO: order flashcards by num_revisions
-
 def create_flashcard(request):
-    print(f'create_flashcard POST: {request.POST.get('front_text')}')
-    print(f'create_flashcard POST: {request.POST.get('back_text')}')
-
     if request.method == 'POST':
         front_text = request.POST.get('front_text')
         back_text = request.POST.get('back_text')
         Flashcard.objects.create(front_text=front_text, back_text=back_text)
+    return redirect('flashcards.index')
 
-    return redirect('index')
-
-def update_flashcard(request, card_id):
-    card = get_object_or_404(Flashcard, id=card_id)
+def update_flashcard(request):
     if request.method == 'POST':
+        card_id = request.POST.get('card_id')
+        card = get_object_or_404(Flashcard, id=card_id)
         card.front_text = request.POST.get('front_text')
         card.back_text = request.POST.get('back_text')
         card.save()
+    return redirect('flashcards.index')
 
-    return redirect('index')
-
-def delete_flashcard(request, card_id):
-    card = get_object_or_404(Flashcard, id=card_id)
+def delete_flashcard(request):
     if request.method == 'POST':
+        card_id = request.POST.get('card_id')
+        card = get_object_or_404(Flashcard, id=card_id)
         card.delete()
+    return redirect('flashcards.index')
 
-    return redirect('index')
-
-def mark_reviewed(request, card_id):
-    card = get_object_or_404(Flashcard, id=card_id)
+def mark_reviewed(request):
     if request.method == 'POST':
+        card_id = request.POST.get('card_id')
+        card = get_object_or_404(Flashcard, id=card_id)
         card.num_revisions += 1
         card.save()
-        return JsonResponse({'success': True, 'revisions': card.num_revisions})
-    return JsonResponse({'success': False})
+    return redirect('flashcards.index')
