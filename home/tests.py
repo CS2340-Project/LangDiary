752c20fd4ab0c30eeaeecf761c0373102489b4c9
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-08 12:19:26 -0400

    feat: add tests for home page

--- home/tests.py
+++ home/tests.py
@@ -1,1 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class HomeViewsTests(TestCase):
+    def test_index_view(self):
+        """Test index view returns correct response with proper template and context"""
+        response = self.client.get(reverse('home.index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+        self.assertEqual(response.context['template_data']['title'], 'LangDiary - Home')
+
+    def test_about_view(self):
+        """Test about view returns correct response with proper template and context"""
+        response = self.client.get(reverse('home.about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/about.html')
+        self.assertEqual(response.context['template_data']['title'], 'LangDiary - About')
