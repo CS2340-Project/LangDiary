4609d035edc20fc3ab04729c3adaf008939a726d
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-08 12:17:49 -0400

    feat: add admin panel for Goal model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from .models import Goal
+
 
 # Customize the User admin display
 class CustomUserAdmin(UserAdmin):
@@ -9,6 +11,31 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('-date_joined',)
 
+
+class GoalAdmin(admin.ModelAdmin):
+    list_display = (
+    'title', 'user', 'target_value', 'current_value', 'unit', 'deadline', 'progress_percentage', 'is_completed')
+    list_filter = ('unit', 'created_at', 'deadline')
+    search_fields = ('title', 'description', 'user__username')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+
+    readonly_fields = ('progress_percentage', 'is_completed')
+
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'title', 'description')
+        }),
+        ('Progress', {
+            'fields': ('target_value', 'current_value', 'unit', 'progress_percentage', 'is_completed')
+        }),
+        ('Timeline', {
+            'fields': ('deadline', 'created_at')
+        }),
+    )
+
+
 # Unregister the default User admin and register our customized version
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Goal, GoalAdmin)
