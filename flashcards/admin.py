dfc0491d9ef61925498cc628fbf8891f483ca692
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-08 11:49:51 -0400

    feat: add admin panel for users and flashcards

--- flashcards/admin.py
+++ flashcards/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Flashcard
 
-# Register your models here.
+
+@admin.register(Flashcard)
+class FlashcardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'front_text_preview', 'back_text_preview', 'num_revisions', 'created_at', 'updated_at')
+    list_filter = ('num_revisions', 'created_at', 'updated_at')
+    search_fields = ('front_text', 'back_text')
+    ordering = ('-updated_at',)
+
+    def front_text_preview(self, obj):
+        return obj.front_text[:50] + "..." if len(obj.front_text) > 50 else obj.front_text
+
+    def back_text_preview(self, obj):
+        return obj.back_text[:50] + "..." if len(obj.back_text) > 50 else obj.back_text
+
+    front_text_preview.short_description = 'Front Text'
+    back_text_preview.short_description = 'Back Text'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+# Customize the User admin display
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
+# Unregister the default User admin and register our customized version
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
