717f056e442cff9ee760a325288fd2456e263f2b
Mark Goltsman <mmgoltsman@mail.ru>
2025-04-09 00:05:22 -0400

    Added view details

--- langlocale/views.py
+++ langlocale/views.py
@@ -5,7 +5,6 @@ from .utils.langlocale import get_data
 def index(request):
     template_data = {'title': 'LangLocales'}
     data = get_data()
-    print(data)
     return render(request, 'langlocale/index.html', {
         'place_data': data
     })
