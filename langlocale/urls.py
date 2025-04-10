35788885f16cc2ddc22a1e1596965497d6747cc9
Mark Goltsman <mmgoltsman@mail.ru>
2025-04-09 23:42:36 -0400

    Favoriting functionality

--- langlocale/urls.py
+++ langlocale/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
  path('', views.index, name='langlocale.index'),
+ path('add_to_favorites', views.AddToFavoritesView.as_view(), name='langlocale.add_to_favorites'),
 ]
--- users/models.py
+++ users/models.py
@@ -94,12 +94,15 @@ class Place(models.Model):
     placeImageUrl = models.URLField()
     placeName = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.placeName} ({self.placeId[:15]}...)"
+
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='favorited_by')
 
     class Meta:
-        unique_together = ('user', 'place')  # P
+        unique_together = ('user', 'place')
 
 @property
 def progress_percentage(self):
