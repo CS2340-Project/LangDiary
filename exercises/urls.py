01c4f85dd7ebfefa211460e0be960fa773d7af57
calvin-k347 <Clark.calvink@gmail.com>
2025-04-20 15:22:57 -0400

    Lan 46 (#36)
    * added option to select deadline and save progress if not complete
    
    * return to exercise by clicking on it
    
    * migration 0023_merge_20250420_1515
    
    * migrations 0024_merge_20250420_1518

--- exercises/urls.py
+++ exercises/urls.py
@@ -4,5 +4,5 @@ app_name = 'exercises'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create_exercise, name='create'),
-    path('create_page/', views.create_page, name="create_page")
+    path('exercise/<int:exercise_id>/', views.create_page, name='create_page')
 ]
