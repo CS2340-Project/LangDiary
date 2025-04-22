5eeddbda4e4e02882d71342832e702da21d40fa2
Daniil Tatarinov <92638932+idkwhttsay@users.noreply.github.com>
2025-04-22 13:36:45 -0400

    ready for deployment (#38)
    * base UI and exercise model
    
    * create new excercise , one excercise per week
    
    * polished ui
    
    * stored responses from form in excercise object
    
    * Added to nav bar
    
    * attempt to fix json path
    
    * fix: upper intermediate level problem + refac: run linter
    
    * feat: prepare for deployment
    
    ---------
    
    Co-authored-by: calvin-k347 <clark.calvink@gmail.com>

--- LangDiary/settings.py
+++ LangDiary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / "home/static",  BASE_DIR / 'exercises/static', BASE_DIR / 'videos/static', BASE_DIR / 'langlocale/static', BASE_DIR / 'users/static']
 
 
