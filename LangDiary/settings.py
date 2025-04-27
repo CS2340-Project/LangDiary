8a7baa1537eb9756caaf5ec1bb3a141b89592393
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-27 14:48:06 -0400

    fix: trusted origins

--- LangDiary/settings.py
+++ LangDiary/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    "*",
+    "https://*",
     "https://*.railway.app",
     "https://*.localhost",
     "https://langdiary.tatarinov.org",
